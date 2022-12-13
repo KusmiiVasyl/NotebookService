b95560d08bcf912308091cdaa5d200892753a2a7
KusmiiVasyl <vasyl.kusmiy@gmail.com>
2022-12-13 22:53:54 +0200

    Initial commit

---
+++ src/main/java/com/delivery/notebookservice/NotebookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.notebookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/delivery/notebookservice/NotebookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.notebookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
